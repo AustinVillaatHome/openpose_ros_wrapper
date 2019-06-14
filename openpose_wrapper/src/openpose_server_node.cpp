069370592e7641496c8feb481c3ec2df8d055dd9
Yuqian Jiang <jiangyuqian@utexas.edu>
2019-06-14 17:42:22 -0500

    disable compression again

--- openpose_wrapper/src/openpose_server_node.cpp
+++ openpose_wrapper/src/openpose_server_node.cpp
@@ -232,7 +232,7 @@ int main(int argc, char *argv[])
     ros::init(argc, argv, "openpose_server");
     ros::NodeHandle nh;
 
-    ros::start();
+    //ros::start();
 
     mp = new MyPublisher;
 
@@ -244,8 +244,9 @@ int main(int argc, char *argv[])
     // TODO: Using compressed stream breaks this
     // perhaps opencv incompatibility
     image_transport::ImageTransport it(nh);
-    image_transport::TransportHints hints("compressed", ros::TransportHints());
-    image_transport::Subscriber subscriber = it.subscribe(FLAGS_image_dir, 1, image_callback, ros::VoidPtr(), hints);
+    //image_transport::TransportHints hints("compressed", ros::TransportHints());
+    //image_transport::Subscriber subscriber = it.subscribe(FLAGS_image_dir, 1, image_callback, ros::VoidPtr(), hints);
+    image_transport::Subscriber subscriber = it.subscribe(FLAGS_image_dir, 1, image_callback);
 
     mp->server = nh.advertiseService("/openpose/GetPersons", add);
 
